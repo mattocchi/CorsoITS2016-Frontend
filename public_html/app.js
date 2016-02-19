4270db6baa4a4542983a787882b37df2c56cfe29
Mirco <amirco@gmail.com>
2016-02-19 09:14:08 +0100

    corretto inizializzazione stringhe

--- public_html/app.js
+++ public_html/app.js
@@ -4,8 +4,8 @@ var x; // dichiara una variabile di nome x
 // i valori sono assegnati con segno =
 x = 0; // ora la variabile x ha valore 0 (Number type)
 x = 0.01; // decimale (Number type sia per real che integer)
-x = “ciao”; // stringa di testo fra doppie virgolette
-x = ‘corso’; // testo racchiuso fra virgoletta singola
+x = "ciao"; // stringa di testo fra doppie virgolette
+x = 'corso'; // testo racchiuso fra virgoletta singola
 x = true;
 x = null; // Null e’ valore speciale, significa senza valore
 x = undefined; // undefined e’ simile a null

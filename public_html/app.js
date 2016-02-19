42a9a8fdf962b24b2874fc758175803a7f900ef1
Mirco <amirco@gmail.com>
2016-02-19 09:11:12 +0100

    inizializzazione variabile

--- public_html/app.js
+++ public_html/app.js
@@ -1,5 +1,11 @@
-// primo  script javascript
-var x = 3;
-var y = 2;
-console.log(x + y);
-console.log("ciao a tutti");
+// la variabile e’ un nome simbolico per un valore
+var x; // dichiara una variabile di nome x
+
+// i valori sono assegnati con segno =
+x = 0; // ora la variabile x ha valore 0 (Number type)
+x = 0.01; // decimale (Number type sia per real che integer)
+x = “ciao”; // stringa di testo fra doppie virgolette
+x = ‘corso’; // testo racchiuso fra virgoletta singola
+x = true;
+x = null; // Null e’ valore speciale, significa senza valore
+x = undefined; // undefined e’ simile a null

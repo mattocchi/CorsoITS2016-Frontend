9857c0df66ef4472ad8cd653b8d5f39da6c4fd9d
Mirco <amirco@gmail.com>
2016-02-22 17:03:06 +0100

    aggiunto esercizio 2

---
+++ public_html/esercizio2.js
@@ -0,0 +1,15 @@
+'use strict';
+/* 
+ * OPERATORI DI CONFRONTO
+ * 
+ *   COMPARARE == UNA VARIABILE INTERA CHE 
+ *   VALE 1 AD UN BOOLEAN CHE VALE TRUE
+ * 
+ */
+
+var numero = 1;
+var booleana = true;
+
+console.log("numero == booleana:" + (numero == booleana));
+// console.log(("numero == booleana:" + numero) == booleana);
+console.log("numero === booleana:" + (numero === booleana));

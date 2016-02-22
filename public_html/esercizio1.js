45b3824f8016fd70a017cc24e4ea0750dc2af928
Mirco <amirco@gmail.com>
2016-02-22 17:02:54 +0100

    aggiunto esercizio 1

---
+++ public_html/esercizio1.js
@@ -0,0 +1,19 @@
+'use strict';
+/* 
+ * concatenazione di stringhe
+ * 
+ * concatenare con operatore +
+ * due variabili di tipo diverso
+ * la variabile "numero" e la variabile "testo"
+ * 
+ */
+var numero = 1;
+var testo = " parola";
+
+console.log(numero + testo);
+
+console.log(2 + 3 + testo);
+console.log(testo + 2 + 3);
+console.log(testo + (2 + 3));
+
+console.log(2 + "3" + testo);
